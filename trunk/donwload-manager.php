1861cd335eb82d08789b63763ee364acd05481b7
codename065 <codename065@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-13 02:40:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/download-manager@204761 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/donwload-manager.php
+++ trunk/donwload-manager.php
@@ -2,14 +2,14 @@
 /**
  * @package Downlodable File Manager
  * @author Shaon
- * @version 1.1
+ * @version 1.2
  */
 /*
 Plugin Name: Downlodable File Manager
 Plugin URI: http://www.intelisoftbd.com
 Description: Manage Downloadable Files
 Author: Shaon
-Version: 1.1
+Version: 1.2
 Author URI: http://www.intelisoftbd.com
 */
 
