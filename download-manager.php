6e3e56ba063ea3e304205ce10babca3405428f84
codename065 <codename065@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-17 10:23:48 +0000

    updated version number only
    git-svn-id: https://plugins.svn.wordpress.org/download-manager/trunk@640442 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- download-manager.php
+++ download-manager.php
@@ -4,7 +4,7 @@ Plugin Name: Download Manager
 Plugin URI: http://www.wpdownloadmanager.com/
 Description: Manage, track and controll file download from your wordpress site
 Author: Shaon
-Version: 2.3.0
+Version: 2.3.1
 Author URI: http://www.wpdownloadmanager.com/
 */
 
